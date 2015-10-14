3860b11f84d99bc620df1da5bd0aa33c16edb62b
islam alzobaydee <islam.alzobaydee@hotmail.com>
2015-10-14 16:48:41 +0200

    Added Project Modeling Diagrams

--- NA.Web/Models/Product/CreateProductViewModel.cs
+++ NA.Web/Models/Product/CreateProductViewModel.cs
@@ -11,19 +11,24 @@ namespace NA.Web.Models.Product
     {
         public string Name { get; set; }
         public decimal Provision { get; set; }
+
         [Display(Name = "Start Price")]
         public decimal StartPrice { get; set; }
+
         [Display(Name="Product Type")]
         public SelectList ProductTypes { get; set; }
         public int ProductTypeId { get; set; }
 
         public SelectList Suppliers { get; set; }
         public Guid? SupplierId { get; set; }
+
         [Display(Name = "Supplier Name")]
         public string SupplierName { get; set; }
 
         public SelectList Designers { get; set; }
         public Guid? DesignerId { get; set; }
+
+
         [Display(Name = "Designer Name")]
         public string DesignerName { get; set; }
 
